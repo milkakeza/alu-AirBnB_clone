31679367bd88de7b056523fe72fc3048bfd3e2bf
kolawizzy <mac@KLD.local>
2025-01-15 22:55:21 +0100

    task4 create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ class BaseModel:
     """
     BaseModel class
     """
-    def __init__(self):
+    def __init__(self, **kwargs):
         """
         Initializes a BaseModel instance
 
@@ -18,9 +18,16 @@ class BaseModel:
         created_at (datetime): datetime object of creation time
         updated_at (datetime): datetime object of last modification time
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """
@@ -42,9 +49,9 @@ class BaseModel:
         Returns a dictionary representation of the BaseModel instance,
         including the class name, id, created_at, and updated_at.
         """
-        obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = self.__class__.__name__
-        obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
-
-        return obj_dict
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        for key, value in new_dict.items():
+            if isinstance(value, datetime):
+                new_dict[key] = value.isoformat()
+        return new_dict
