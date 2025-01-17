0edea6cdd193826db458509ae4faafc2ccb330ee
milkakeza <m.isingizwe1@alustudent.com>
2025-01-17 02:32:25 +0200

    TestFileStorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -9,6 +9,12 @@ import unittest
 from datetime import datetime
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class TestFileStorage_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the FileStorage class."""
@@ -61,10 +67,28 @@ class TestFileStorage_methods(unittest.TestCase):
 
     def test_new(self):
         bm = BaseModel()
+        usr = User()
+        ste = State()
+        plc = Place()
+        cty = City()
+        amn = Amenity()
+        rvw = Review()
         
         models.storage.new(bm)
         self.assertIn("BaseModel." + bm.id, models.storage.all().keys())
         self.assertIn(bm, models.storage.all().values())
+        self.assertIn("User." + usr.id, models.storage.all().keys())
+        self.assertIn(usr, models.storage.all().values())
+        self.assertIn("State." + ste.id, models.storage.all().keys())
+        self.assertIn(ste, models.storage.all().values())
+        self.assertIn("Place." + plc.id, models.storage.all().keys())
+        self.assertIn(plc, models.storage.all().values())
+        self.assertIn("City." + cty.id, models.storage.all().keys())
+        self.assertIn(cty, models.storage.all().values())
+        self.assertIn("Amenity." + amn.id, models.storage.all().keys())
+        self.assertIn(amn, models.storage.all().values())
+        self.assertIn("Review." + rvw.id, models.storage.all().keys())
+        self.assertIn(rvw, models.storage.all().values())
 
     def test_new_with_args(self):
         with self.assertRaises(TypeError):
@@ -72,12 +96,30 @@ class TestFileStorage_methods(unittest.TestCase):
 
     def test_save(self):
         bm = BaseModel()
+        usr = User()
+        ste = State()
+        plc = Place()
+        cty = City()
+        amn = Amenity()
+        rvw = Review()
         models.storage.new(bm)
+        models.storage.new(usr)
+        models.storage.new(ste)
+        models.storage.new(plc)
+        models.storage.new(cty)
+        models.storage.new(amn)
+        models.storage.new(rvw)
         models.storage.save()
         save_text = ""
         with open("file.json", "r") as f:
             save_text = f.read()
             self.assertIn("BaseModel." + bm.id, save_text)
+            self.assertIn("User." + usr.id, save_text)
+            self.assertIn("State." + ste.id, save_text)
+            self.assertIn("Place." + plc.id, save_text)
+            self.assertIn("City." + cty.id, save_text)
+            self.assertIn("Amenity." + amn.id, save_text)
+            self.assertIn("Review." + rvw.id, save_text)
 
     def test_save_with_arg(self):
         with self.assertRaises(TypeError):
@@ -85,11 +127,29 @@ class TestFileStorage_methods(unittest.TestCase):
 
     def test_reload(self):
         bm = BaseModel()
+        usr = User()
+        ste = State()
+        plc = Place()
+        cty = City()
+        amn = Amenity()
+        rvw = Review()
         models.storage.new(bm)
+        models.storage.new(usr)
+        models.storage.new(ste)
+        models.storage.new(plc)
+        models.storage.new(cty)
+        models.storage.new(amn)
+        models.storage.new(rvw)
         models.storage.save()
         models.storage.reload()
         objs = FileStorage._FileStorage__objects
         self.assertIn("BaseModel." + bm.id, objs)
+        self.assertIn("User." + usr.id, objs)
+        self.assertIn("State." + ste.id, objs)
+        self.assertIn("Place." + plc.id, objs)
+        self.assertIn("City." + cty.id, objs)
+        self.assertIn("Amenity." + amn.id, objs)
+        self.assertIn("Review." + rvw.id, objs)
 
     def test_reload_with_arg(self):
         with self.assertRaises(TypeError):
