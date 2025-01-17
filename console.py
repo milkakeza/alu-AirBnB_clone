cbf8037b648f0661db50943addf9176cdcb3293c
root <koladeade>
2025-01-16 16:57:18 -0800

    task10

--- console.py
+++ console.py
@@ -5,6 +5,11 @@ import shlex
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """A command line interface (CLI) for the HBNB project."""
