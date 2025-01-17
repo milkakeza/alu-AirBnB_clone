0d63302e288761f4cc48b2315753081248d904a5
milkakeza <m.isingizwe1@alustudent.com>
2025-01-17 11:21:44 -0800

    Console file

--- console.py
+++ console.py
@@ -61,6 +61,7 @@ class HBNBCommand(cmd.Cmd):
                 print(objects[key])
             else:
                 print("** no instance found **")
+    
     def do_destroy(self, arg):
         """Deletes an instance based on class name and id."""
         command = shlex.split(arg)
@@ -129,4 +130,5 @@ class HBNBCommand(cmd.Cmd):
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+
 
